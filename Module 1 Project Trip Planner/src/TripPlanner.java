b64429dbcdbefeb1085c5f380be3c666fc8884fb
George Lopez <50560354+GeorgeChristopherLopez@users.noreply.github.com>
2019-11-21 10:57:39 -0500

    Add files via upload

--- Module 1 Project Trip Planner/src/TripPlanner.java
+++ Module 1 Project Trip Planner/src/TripPlanner.java
@@ -22,20 +22,23 @@ public class TripPlanner {
         System.out.println(" ");
         Scanner input = new Scanner(System.in);
         System.out.print("How many days are you going to spend traveling? ");
-        String days = input.nextLine();
+        int days = input.nextInt();
         System.out.print("How much money, in USD, are you planning to spend on your trip? ");
-        String budget = input.nextLine();
+        double dollars = input.nextDouble();
         System.out.print("What is the three letter currency symbol for your travel destination? ");
-        String currency = input.nextLine();
+        String currency = input.next();
         System.out.print("How many " + currency + " are there in 1 USD? ");
-        String conversionRate = input.nextLine();
-
+        double conversionRate = input.nextDouble();
+        int hours = days * 24;
+        int minutes = hours * 60;
+        double dollarsPerDay = dollars / days;
+        double dollarsConverted = dollars * conversionRate;
+        double dollarsPerDayConverted = dollarsConverted / days;
         System.out.println(" ");
         System.out.println(" ");
-
-
-
-
+        System.out.println("If you are traveling for " + days + " days that is the same as "+hours +" hours or " + minutes + " minutes");
+        System.out.println("If you are going to spend $" + dollars + " USD that means per day you can spend up to "+ dollarsPerDay +"USD");
+        System.out.println("Your total budget in "+ currency+ " is "+ dollarsConverted +" " + currency + ", which per day is "+ dollarsPerDayConverted+ " "+currency);
 
     }
 }
