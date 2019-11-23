93e2bb07a886fe1ab1e58fa3e7d697a7d70c4554
George Lopez <50560354+GeorgeChristopherLopez@users.noreply.github.com>
2019-11-22 22:24:04 -0500

    created all methods

--- Module 1 Project Trip Planner/src/TripPlanner.java
+++ Module 1 Project Trip Planner/src/TripPlanner.java
@@ -39,6 +39,32 @@ public class TripPlanner {
         System.out.println("If you are traveling for " + days + " days that is the same as "+hours +" hours or " + minutes + " minutes");
         System.out.println("If you are going to spend $" + dollars + " USD that means per day you can spend up to "+ dollarsPerDay +"USD");
         System.out.println("Your total budget in "+ currency+ " is "+ dollarsConverted +" " + currency + ", which per day is "+ dollarsPerDayConverted+ " "+currency);
+        TimeDifference();
 
     }
+        public static void TimeDifference(){
+            System.out.println("****************");
+            System.out.println(" ");
+            System.out.println(" ");
+            Scanner input = new Scanner(System.in);
+            System.out.print("What is the time difference, in hours, between your home and your destination? ");
+            int timeDif = input.nextInt();
+            int midNiteThere = timeDif + 0;
+            int noonThere = timeDif + 12;
+            System.out.println("That means that when it is midnight at home it will be "+ midNiteThere +":00 in your travel destination");
+            System.out.println("and when it is noon at home it will be " + noonThere+":00");
+            CountryArea();
+        }
+        public static void CountryArea(){
+            System.out.println("****************");
+            System.out.println(" ");
+            System.out.println(" ");
+            Scanner input = new Scanner(System.in);
+            System.out.print("What is the square of your destination country in km2? ");
+            double areaInKM = input.nextInt();
+            double areaInMiles = areaInKM * .621371;
+            System.out.println("In miles2 that is " + areaInMiles);
+            System.out.println("****************");
+        }
+
 }
